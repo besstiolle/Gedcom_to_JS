fb1a4ea0f806712743e4a1f4272a05bd720ca14b
Danezis <kdanezis@gmail.com>
2022-07-10 22:39:06 +0200

    :recycle: remove node_module

--- node_modules/@svgdotjs/svg.js/spec/spec/types/Base.js
+++
@@ -1,9 +0,0 @@
-import Base from '../../../src/types/Base.js'
-
-const { any } = jasmine
-
-describe('Base.js', () => {
-  it('holds the base class', () => {
-    expect(new Base).toEqual(any(Base))
-  })
-})
--- node_modules/@svgdotjs/svg.js/src/svg.js
+++
@@ -1,9 +0,0 @@
-import * as svgMembers from './main.js'
-import { makeInstance } from './utils/adopter.js'
-
-// The main wrapping element
-export default function SVG (element) {
-  return makeInstance(element)
-}
-
-Object.assign(SVG, svgMembers)
